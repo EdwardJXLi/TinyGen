eb691b40da026cde995011bfd891ae1f95470b1d
Edward Li <radioactivehydra@gmail.com>
2024-02-28 21:57:43 -0800

    Add more comment to OpenAI class

--- processes/openai.py
+++ processes/openai.py
@@ -10,6 +10,13 @@ from constants import DEFAULT_GPT_MODEL
 
 class OpenAIInteraction:
     def __init__(self, task, api_key: str):
+        """
+        Creates an OpenAIInteraction class, which handles all the communication and interaction with the OpenAI API.
+
+        Parameters:
+            task (TinyGenTask): The task object to use for logging.
+            api_key (str): The API key to use for the OpenAI API.
+        """
         # Initialize the OpenAI API
         self.task = task
         self.logger: Logger = task.logger
@@ -27,6 +34,19 @@ class OpenAIInteraction:
         response_id: int = 0,
         **kwargs
     ) -> ChatCompletionMessage:
+        """
+        Generate a response from the OpenAI API.
+
+        Parameters:
+            messages (Iterable[ChatCompletionMessageParam]): The messages to use as context for the response. This is an iterable of ChatCompletionMessageParam objects
+            model (str): The model to use for the response. Defaults to the default GPT model.
+            temperature (float): The temperature to use for the response. Defaults to 0.4.
+            response_id (int): The index of the response to return. Defaults to 0.
+            **kwargs: Additional keyword arguments to pass to the OpenAI API.
+
+        Returns:
+            ChatCompletionMessage: The response from the OpenAI API.
+        """
         self.logger.info(f"[TinyGen > GPT] Generating response from OpenAI ({model})...")
 
         # Generate a response from the OpenAI API
@@ -44,6 +64,7 @@ class OpenAIInteraction:
         return response.choices[response_id].message
 
 
+# Available functions for the OpenAI interaction
 FUNCTIONS = [
     {
         "type": "function",
